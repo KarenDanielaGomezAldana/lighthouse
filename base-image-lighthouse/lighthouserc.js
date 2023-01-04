79a096cd39afe6050cb122240f8d13161e9acb44
Karen Daniela Gómez Aldana <63369380+KarenDanielaGomezAldana@users.noreply.github.com>
2023-01-04 14:26:03 -0500

    Add files via upload

---
+++ base-image-lighthouse/lighthouserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "npm run start",
+      numberOfRuns: 3,
+      url: ["https://www.ciencuadras.com/"],
+      //chromePath: "/usr/bin/google-chrome",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 99 }],
+        "categories:accessibility": ["error", { minScore: 99 }],
+        "categories:best-practices": ["error", { minScore: 99 }],
+        "categories:seo": ["error", { minScore: 99 }],
+        "categories.pwa": "off",
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
